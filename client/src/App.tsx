38c94fd16579c18f9c9103b77ddf2e409276cf23
VincenzoParrinelli <vincenzoparrinelli01@gmail.com>
2023-02-15 18:41:18 +0100

    feat(fileList): basic styling

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import Header from './Components/Header'
 import FilesDragger from './Components/FilesDragger'
+import FilesList from './Components/FilesList'
 
 function App() {
 
   return (
     <div className="App">
       <Header />
-      <FilesDragger/>
+      <FilesDragger />
+      <FilesList />
     </div>
   )
 }
