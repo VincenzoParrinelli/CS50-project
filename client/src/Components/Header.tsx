0e4602cc6887c4650a2b17b347c776f28aa667d8
VincenzoParrinelli <vincenzoparrinelli01@gmail.com>
2023-02-15 16:54:47 +0100

    feat(scss): styling header

---
+++ client/src/Components/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <div className='header'>
+            <h2 className="heading heading--2">UPLOAD FILES</h2>
+
+            <div className="header__code-container">
+                <h3 className="heading heading--3">YOUR CODE:</h3>
+                <span className="header__code">CODEASASASASAASASASASASASASASAA</span>
+            </div>
+
+            <button className="btn">NEW CODE</button>
+        </div>
+    )
+}
