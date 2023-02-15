f8a8a061e5fd007d822454a8ccc01867db86714e
VincenzoParrinelli <vincenzoparrinelli01@gmail.com>
2023-02-15 19:15:05 +0100

    feat(filesDragger): on drag styling

--- client/src/Components/FilesDragger.tsx
+++ client/src/Components/FilesDragger.tsx
@@ -1,16 +1,20 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { ReactComponent as UploadFileIcon } from "../assets/Images/upload-file.svg"
 
 export default function FilesDragger() {
 
+  const draggableAreaRef = useRef(null) as React.MutableRefObject<HTMLDivElement | null>
+
   const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
     e.preventDefault()
 
+    draggableAreaRef.current!.classList.add("files-dragger__drag-and-drop--on-drag")
   }
 
   const handleDragLeave = (e: React.DragEvent<HTMLDivElement>) => {
     e.preventDefault()
 
+    draggableAreaRef.current!.classList.remove("files-dragger__drag-and-drop--on-drag")
   }
 
   const handleOnDrop = (e: React.DragEvent<HTMLDivElement>) => {
@@ -21,19 +25,17 @@ export default function FilesDragger() {
   return (
     <div className='files-dragger'>
 
-      <div className='files-dragger__container'>
-        <div
-          className="files-dragger__drag-and-drop"
-          onDragOver={e => handleDragOver(e)}
-          onDragLeave={e => handleDragLeave(e)}
-          onDrop={e => handleOnDrop(e)}
-        >
+      <div className='files-dragger__drag-and-drop'
+        ref={draggableAreaRef}
+        onDragOver={e => handleDragOver(e)}
+        onDragLeave={e => handleDragLeave(e)}
+        onDrop={e => handleOnDrop(e)}
+      >
 
-          <UploadFileIcon className='files-dragger__upload-icon' />
+        <UploadFileIcon className='files-dragger__upload-icon' />
 
-          <p className="files-dragger__paragraph">Drag & drop</p>
+        <p className="files-dragger__paragraph">Drag & drop</p>
 
-        </div>
 
       </div>
 
