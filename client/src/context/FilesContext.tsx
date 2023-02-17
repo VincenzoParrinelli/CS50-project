b2e700cf091e921dabbcd36412db66b660f46b6b
VincenzoParrinelli <vincenzoparrinelli01@gmail.com>
2023-02-17 21:09:31 +0100

    fix(contexts): illegal constructor in filesContext

--- client/src/context/FilesContext.tsx
+++ client/src/context/FilesContext.tsx
@@ -1,14 +1,22 @@
 import { useState, createContext, PropsWithChildren } from "react"
 import { FilesInterface } from '../ts/interfaces/filesInterface'
 
-const FilesContext = createContext<FilesInterface>({ files: new FileList(), setFiles: () => {}}) 
+// Create a mock object that implements the FileList interface and use that as the default value 
+// In order to avoid the illegal constructor error, because FileList is an interface and cannot be directly instantiated.
+const initialFiles: FileList = {
+    length: 0,
+    item: () => null,
+    [Symbol.iterator]: function* () { }
+}
+
+const FilesContext = createContext<FilesInterface>({ files: initialFiles, setFiles: () => { } })
 
 export default FilesContext
 
 // FilesContextProvider initialization
 export function FilesContextProvider({ children }: PropsWithChildren) {
 
-    const [files, setFiles] = useState(new FileList())
+    const [files, setFiles] = useState(initialFiles)
 
     return (
         <FilesContext.Provider value={{ files, setFiles }}>
