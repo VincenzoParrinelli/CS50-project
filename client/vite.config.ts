e25efaab3d0179097a64d2cc9b5f74893b992310
VincenzoParrinelli <vincenzoparrinelli01@gmail.com>
2023-02-22 18:19:41 +0100

    fix(vite.config): correct background.js compilation

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -7,11 +7,26 @@ export default defineConfig({
   plugins: [viteSvgr(), react()],
 
   build: {
+    outDir: 'dist',
+
     rollupOptions: {
       input: {
         main: './index.html',
-        background: './public/background.ts',
+        background: './src/background.ts',
       },
+
+      output: {
+        chunkFileNames: 'assets/[name].js',
+        entryFileNames(chunkInfo) {
+
+          // Conditional output path for multiple entry file one for main and one for background
+          // This is needed so background.js can be compiled into dist root folder and main.js into assets folder
+          // Without this all the files would be compiled into assets.
+          if (chunkInfo.name === "main") return "assets/main.js"
+          if (chunkInfo.name === "background") return "background.js"
+
+        },
+      }
     },
   },
 })
